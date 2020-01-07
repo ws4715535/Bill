35f5bbb2bd296ea56ad1acc6000c59fd38e7600b
shuai.wang <>
2020-01-07 13:44:05 +0800

    init

---
+++ Bill/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bill
+//
+//  Created by shuai.wang on 2020/1/7.
+//  Copyright © 2020 Thoughtworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
