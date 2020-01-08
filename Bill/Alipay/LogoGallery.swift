038fdfd2f83c8bb0d41922010d2306e963498056
shuai.wang <624412421@qq.com>
2020-01-08 17:28:37 +0800

    add path manager

---
+++ Bill/Alipay/LogoGallery.swift
@@ -0,0 +1,25 @@
+//
+//  LogoGallery.swift
+//  Bill
+//
+//  Created by shuai.wang on 2020/1/8.
+//  Copyright © 2020 Thoughtworks. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class LogoGallery: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+    
+    fileprivate func loadLogos() {
+        
+        let path = URL(fileURLWithPath: "./")
+        print(path)
+        
+        
+    }
+}
