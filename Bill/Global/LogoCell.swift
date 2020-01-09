fed4fafd59439233c51029d37d08fa39a7232334
shuai.wang <624412421@qq.com>
2020-01-09 14:16:31 +0800

    add logo images

---
+++ Bill/Global/LogoCell.swift
@@ -0,0 +1,18 @@
+//
+//  LogoCell.swift
+//  Bill
+//
+//  Created by shuai.wang on 2020/1/9.
+//  Copyright © 2020 Thoughtworks. All rights reserved.
+//
+
+import UIKit
+
+class LogoCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
